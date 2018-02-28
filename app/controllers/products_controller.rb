b4f60bcd2f1964efda22e7c02b5e3937a8390f38
aubanuet <aubanuet@gmail.com>
2018-02-28 13:34:31 -0500

    all show pages working, need to change path to show after product is created

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,31 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def show
+    @product = Product.find(params[:id])
+
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create
+      @product.name = params[:product][:name]
+      @product.description = params[:product][:description]
+      @product.price = params[:product][:price]
+        if @product.save
+          redirect_to products_path
+        else
+          render :new
+      end
+  end
+
+  def edit
+  end
+
+
+
 end
