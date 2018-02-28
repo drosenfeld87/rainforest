1907cfa8b6fb44573002c0ca03f06708b7fc8334
aubanuet <aubanuet@gmail.com>
2018-02-28 17:18:30 -0500

    complete nested routes for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,4 @@
 class Review < ApplicationRecord
   belongs_to :product
-
+  validates :body, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :products
   root 'products#index'
+  resources :products do
+    resources :reviews, except: [:index, :new, :show]
+  end
 end
