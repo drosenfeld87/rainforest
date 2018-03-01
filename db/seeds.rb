e41f2092a1ebf2e8b20275b26b9b9a77975cbad5
debbie <drosenfeld87@gmail.com>
2018-03-01 18:19:10 -0500

    updated seeds file with 5 times do

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Product.create(name: "iphone", description: "smartphone", price: 10000)
+names = ["iphone", "mouse", "lamp", "laptop", "coffee_mug"]
+descriptions =["s", "f", "e", "w", "i"]
+prices = [1000, 500, 3, 5, 6]
+
+5.times do |i|
+  Product.create(
+    name: names.sample,
+    description: descriptions.sample,
+    price_in_cents: prices.sample
+  )
+end
